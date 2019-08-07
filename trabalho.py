ea3d169d1fd8a034802abdfbbba77074a7fca786
claudiocsjunior <claudio_junior15@github>
2019-08-07 00:06:14 -0300

    Finaliza trabalho

--- trabalho.py
+++ trabalho.py
@@ -1,34 +1,48 @@
 import os
+import time
+import signal
 
 
-def child():
+def filho():
    os._exit(0)  
 
-def parent():
-    childs = [] 
-    
+def pai(filhos):
     for x in range(0, 4):
-        newpid = os.fork()
-        if newpid == 0:
-            child()
+        novoPID = os.fork()
+        if novoPID == 0:
+            filho()
         else:
-            pids = (os.getpid(), newpid)
-            childs.append(newpid)
-            print("parent: %d, child: %d\n" % pids)
+            pids = (os.getpid(), novoPID)
+            filhos.append(novoPID)
+            print("Pai: %d, filho: %d\n" % pids)
+    return filhos
+            
     
-def printar_filhos(filhos)
+def printar_filhos(filhos):
     print(filhos)
 
-
-
-
-    while True:
-        reply = input("q for quit / c for new fork\n")
-        if reply == 'c': 
-            continue
+#comando para listar os filhos: ps --ppid PID_DO_PAI
+filhos = [] 
+filhos = pai(filhos)
+while True:
+    resultado = input(" s - suicidar \n k - matar um processo \n l - listar processos filhos\n")
+    if resultado == 'k': 
+        printar_filhos(filhos)  
+        idFilho = input("\nInforme o processo que quer matar:\n")
+        if(int(idFilho) in filhos):
+            #os.kill(int(idFilho), signal.SIGKILL)
+            os.waitpid(int(idFilho),9)
+            filhos.remove(int(idFilho))
+            print('\nFilho removido com sucesso.')
         else:
-            break
+            print('\nId não encontrado. Tente novamente.')
+        continue
+    elif resultado == 'l':
+        printar_filhos(filhos)    
+        continue
+    else:
+        break
 
-parent()
+
 
 
